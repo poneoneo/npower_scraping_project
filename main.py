bdb6e3513fbdbccce285722ce4c64b9c26208112
poneoneo <onealzero@gmail.com>
2025-01-13 23:59:36 -0500

    feature: lets user decide which youtube chanel to scrape by providing yotube channel name .

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from playwright.sync_api import Browser
 from parser import parse_html, create_videos_dict ,convert_to_dataframe, remove_empty_values , save_to_csv
 import datahorse as dh
 
-def run(playwright):
+def run(playwright, channel_name: str):
     browser : Browser = playwright.chromium.launch(headless=True)  # ouverture du navigateur sans interface graphique
     page = browser.new_page()
-    page.goto('https://www.youtube.com/@JustinSung/videos')  # Aller sur la page de la chaine youtube
+    page.goto(f'https://www.youtube.com/{channel_name}/videos')  # Aller sur la page de la chaine youtube
     page.mouse.down()
     page.locator("#contents").scroll_into_view_if_needed()
     contents = page.locator("#contents")
@@ -21,8 +21,13 @@ def run(playwright):
 #     return content
 
 if __name__ == '__main__':
+    at_channel_name =input("Enter the name of the channel you want to scrape(it should start with a `@` sign ): ")
     with sync_playwright() as playwright:
-        page_content = run(playwright)
+        if not at_channel_name.startswith("@"):
+            print("The channel name is invalid add the `@` sign before the channel name")
+        if at_channel_name == "":
+            exit()
+        page_content = run(playwright, at_channel_name)
 
     parsed_content  = parse_html(page_content)
     videos_dict = create_videos_dict(parsed_content)
